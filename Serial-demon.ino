a209e7adf4f0dddbc12d2be25ba65f933912ee52
kenlui78 <kenlui78@yahoo.com>
2022-05-09 14:13:09 +0800

    added send to demon

--- Serial-demon.ino
+++ Serial-demon.ino
@@ -1,4 +1,6 @@
 #include"remoteButton.h";
+#include <arduino-timer.h>
+
 HardwareSerial mySerial(1);
 
 void setup() {
@@ -8,15 +10,28 @@ void setup() {
 
 void loop() {
   RemoteButton* original = new RemoteButton;
-  original->r2 = 128;
+  original->r2 = 0;
   char* originalChar = static_cast<char*>(static_cast<void*>(original));
   //Create an char pointer and make it point to the object.
   char *buffer = new char[sizeof(RemoteButton)];
   char ch;
   int state = 0;
   int i = 0;
+  char header[] = { 6, 2 };
+  char footer[] = { 6, 3 };
+
+  long int t1 = millis();
+
   Serial.println("Program start.");
     while (true) {
+        if (millis() - t1 > 1000){
+          mySerial.write(header, sizeof(header));
+          mySerial.write(originalChar, sizeof(RemoteButton));
+          mySerial.write(footer, sizeof(footer));
+          original->r2++;
+          t1 = millis();
+          //Serial.println(millis());
+        }
         if (mySerial.available() > 0) {
             mySerial.read(&ch, 1);
         switch (state) {
@@ -28,6 +43,7 @@ void loop() {
             case 1:
             if (ch == 2) {
                 state++;
+                //Serial.printf("head\r\n");
                 i = 0;
             }else if (ch == 6) {
             }else {
